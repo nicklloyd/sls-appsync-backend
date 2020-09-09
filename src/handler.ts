de902a2e4c46381f0d9d0cc11ee0beece286a225
nick <nick@playpass.be>
2020-09-09 11:06:33 +0200

    get hello world running

--- src/handler.ts
+++ src/handler.ts
@@ -1,16 +1,19 @@
-import { APIGatewayProxyHandler } from 'aws-lambda'
-import 'source-map-support/register'
+export const graphql = async (
+  event: unknown,
+  context: unknown,
+  info: unknown
+): Promise<unknown> => {
+  console.log(event)
+  console.log(context)
+  console.log(info)
 
-export const hello: APIGatewayProxyHandler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-        input: event
-      },
-      null,
-      2
-    )
+  let response
+  try {
+    response = 'hello world from appsync'
+  } catch (err) {
+    console.log(err)
+    return err
   }
+
+  return response
 }
